209339cd84812ac469335fc31082cd2e230ba95b
Stealth-mode-OFF <chatujsgpt@gmail.com>
2026-01-13 13:46:43 +0000

    Add SEO/share utilities, analytics helpers, and env docs

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App";
 import { AuthProvider } from "./app/auth/AuthContext";
+import { initPostHog } from "./utils/analytics";
 import "./styles/index.css";
 import "./utils/i18n";
+
+// Initialize PostHog
+initPostHog();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
