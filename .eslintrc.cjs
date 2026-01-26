81497e6cc8a967dceca01a57f54c0eabbfeeab40
Stealth-mode-OFF <chatujsgpt@gmail.com>
2026-01-26 23:01:02 +0000

    chore(tooling): add lint, format, and typecheck tooling

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "react-refresh", "simple-import-sort"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react-refresh/recommended",
+    "prettier",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+  ignorePatterns: ["dist", "node_modules", "coverage", "supabase", "tests"],
+};
